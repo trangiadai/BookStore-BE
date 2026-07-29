2b87610a982337ec3f7797d19ca3585f3520b318
DAITG-PC\Intern <tgdai119@gmail.com>
2026-07-29 09:00:29 +0700

    init project

---
+++ src/main/java/com/tgd/BookStoreWebApplication.java
@@ -0,0 +1,13 @@
+package com.tgd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreWebApplication.class, args);
+	}
+
+}
